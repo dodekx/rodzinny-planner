0b170562c6cf613112bee0757aa89b8d69780a40
Dominik Kajzar <dominikkajzar@gmail.com>
2026-05-04 16:13:43 +0200

    feat: configure firebase with project credentials and install dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVwrgO49WF2mafI4VZJjShfo7CRqxMCTY",
+  authDomain: "rodzinny-planner.firebaseapp.com",
+  projectId: "rodzinny-planner",
+  storageBucket: "rodzinny-planner.firebasestorage.app",
+  messagingSenderId: "568084161910",
+  appId: "1:568084161910:web:408b75a9dfc68853820657"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ]
 };
